c2271619d112acd24222f46a044984861569bcb8
Emmanuel Madi <42469602+MadiEmmanuel@users.noreply.github.com>
2019-05-03 16:19:02 +0200

    Update mali.py

--- mali.py
+++ mali.py
@@ -67,91 +67,212 @@ from plotly.offline import download_plotlyjs, init_notebook_mode, plot, iplot
 data = pd.read_csv("provincial-time-series.csv")
 data.dropna()
 
-EC = data[data.government == 'Eastern Cape']
-money = EC1.value
-prov = EC1.department
+def EC(*args):
+    data = pd.read_csv("provincial-time-series.csv")
+    data.dropna()
 
-data = [go.Bar(
+    EC = data[data.government == 'Eastern Cape']
+    money = EC1.value
+    prov = EC1.department
+    
+    data = [go.Bar(
             x=prov,
             y=money
-    )]
+            )]
 
-iplot(data, filename='basic-bar')
+    iplot(data, filename='basic-bar')
+    
+print(EC()) 
+
+
+GP1 = data[data.government == 'Gauteng']
+GP = df[GP1]
+
+def GP(**args):
+    import pandas as pd
+    import plotly.graph_objs as go
+    from plotly.offline import download_plotlyjs, init_notebook_mode, plot, iplot
+
+    data = pd.read_csv("provincial-time-series.csv")
+    data.dropna()
+
+    GP1 = data[data.government == 'Gauteng']
+    money = GP1.value
+    prov = GP1.department
+
+    data = [go.Bar(
+            x=prov,
+            y=money
+         )]
+
+    iplot(data, filename='basic-bar')
+    
+print(GP()) 
+
+# In[ ]:
+WC1 = data[data.government == 'Western Cape']
+GP = df[WP1]
+# In[ ]:
+
+def WP(**args):
+    import pandas as pd
+    import plotly.graph_objs as go
+    from plotly.offline import download_plotlyjs, init_notebook_mode, plot, iplot
+
+    data = pd.read_csv("provincial-time-series.csv")
+    data.dropna()
+
+    WC1 = data[data.government == 'Western Cape']
+    money = WC1.value
+    prov = WC1.department
+
+    data = [go.Bar(
+            x=prov,
+            y=money
+         )]
+
+    iplot(data, filename='basic-bar')
+    
+print(WP()) 
+
+# In[ ]:
+
+FS1 = data[data.government == 'Free State']
+FS = df[FS1]
+def FS(**args):
+    import pandas as pd
+    import plotly.graph_objs as go
+    from plotly.offline import download_plotlyjs, init_notebook_mode, plot, iplot
+
+    data = pd.read_csv("provincial-time-series.csv")
+    data.dropna()
+
+    FS1 = data[data.government == 'Free State']
+    money = FS1.value
+    prov = FS1.department
+
+    data = [go.Bar(
+            x=prov,
+            y=money
+         )]
+
+    iplot(data, filename='basic-bar')
+    
+print(FS())
+
+
+# In[ ]:
+
+NW1 = data[data.government == 'North West']
+NW = df[NW1]
+
+def NW(**args):
+    import pandas as pd
+    import plotly.graph_objs as go
+    from plotly.offline import download_plotlyjs, init_notebook_mode, plot, iplot
+
+    data = pd.read_csv("provincial-time-series.csv")
+    data.dropna()
+
+    NW1 = data[data.government == 'North West']
+    money = NW1.value
+    prov = NW1.department
+
+    data = [go.Bar(
+            x=prov,
+            y=money
+         )]
+
+    iplot(data, filename='basic-bar')
+    
+print(FS())
+
+# In[ ]:
+
+KZN1 = data[data.government == 'KwaZulu-Natal']
+NW = df[KZN1]
+
+# In[ ]:
+
+
+def Mp(**args):
+    import pandas as pd
+    import plotly.graph_objs as go
+    from plotly.offline import download_plotlyjs, init_notebook_mode, plot, iplot
+
+    data = pd.read_csv("provincial-time-series.csv")
+    data.dropna()
+
+    Mp1 = data[data.government == 'Mpumalanga']
+    money = Mp1.value
+    prov = Mp1.department
+
+    data = [go.Bar(
+            x=prov,
+            y=money
+         )]
+
+    iplot(data, filename='basic-bar')
+    
+print(Mp())
+
+# In[ ]:
+
+L1 = data[data.government == 'Limpopo']
+NW = df[L1]
+
+# In[ ]:
+
+def L(**args):
+    import pandas as pd
+    import plotly.graph_objs as go
+    from plotly.offline import download_plotlyjs, init_notebook_mode, plot, iplot
+
+    data = pd.read_csv("provincial-time-series.csv")
+    data.dropna()
+
+    L1 = data[data.government == 'Limpopo']
+    money = L1.value
+    prov = L1.department
+
+    data = [go.Bar(
+            x=prov,
+            y=money
+         )]
+
+    iplot(data, filename='basic-bar')
+    
+print(L())
 
 
 # In[ ]:
 
 
-
-
-
-# In[14]:
-
-
-GP = data[data.government == 'Gauteng']
-
+NC1 = data[data.government == 'Northen Cape']
+NC = df[NC1]
 
 # In[ ]:
+def L(**args):
+    import pandas as pd
+    import plotly.graph_objs as go
+    from plotly.offline import download_plotlyjs, init_notebook_mode, plot, iplot
 
+    data = pd.read_csv("provincial-time-series.csv")
+    data.dropna()
 
+    L1 = data[data.government == 'Northen Cape']
+    money = L1.value
+    prov = L1.department
 
+    data = [go.Bar(
+            x=prov,
+            y=money
+         )]
 
+    iplot(data, filename='basic-bar')
+    
+print(L())
 
-# In[ ]:
-
-
-
-
-
-# In[ ]:
-
-
-
-
-
-# In[ ]:
-
-
-WC = data[data.government == 'Western Cape']
-
-
-# In[ ]:
-
-
-FS = data[data.government == 'Free State']
-
-
-# In[ ]:
-
-
-NW = data[data.government == 'North West']
-
-
-# In[ ]:
-
-
-KZN = data[data.government == 'KwaZulu-Natal']
-
-
-# In[ ]:
-
-
-Mp = data[data.government == 'Mpumalanga']
-
-
-# In[ ]:
-
-
-L = data[data.government == 'Limpopo']
-
-
-# In[ ]:
-
-
-NC = data[data.government == 'Northen Cape']
-
-
-# In[ ]:
 
 
 
